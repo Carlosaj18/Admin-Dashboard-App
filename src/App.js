e170dd51361589d422bab90e0540468a65446463
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-01-26 12:23:10 -0500

    Created the Dashboard React App

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import SideBar from "./scenes/global/SideBar";
+import TopBar from "./scenes/global/TopBar";
+import Dashboard from "./scenes/dashboard";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      {/** Color Context */}
+      <ThemeProvider theme={theme}>
+        {/** Theme Provider for MUI */}
+        <CssBaseline /> {/** Reset css values  */}
+        <div className="app">
+          <SideBar isSidebar={isSidebar} />
+          <main className="content">
+            <TopBar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,14 @@
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
+};
+
+export default Dashboard;
