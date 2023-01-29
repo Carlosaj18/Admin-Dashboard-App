e2d8b0c4ed02561b5c43909500d722c044b22b74
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-01-29 13:35:21 -0500

    Created bar scence with its component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
+import Bar from "./scenes/bar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -30,6 +31,7 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
             </Routes>
           </main>
         </div>
