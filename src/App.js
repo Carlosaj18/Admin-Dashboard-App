e9efd9559de71a27a8af74f0a2a5190b8128f3c3
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-01-30 10:18:41 -0500

    Created calendar scences and solve conflict verion

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar/calendar";
 import Bar from "./scenes/bar";
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
               <Route path="/bar" element={<Bar />} />
             </Routes>
           </main>
